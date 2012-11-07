07589c131c465acb71cca493a7df357e5ff0df41
Romain Tartière <romain@blogreen.org>
2012-11-07 16:27:24 +0100

    Create Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalendarSigabrtOrg::Application.config.secret_token = '8fbfd53b50f9b845ae8d36a2bf97bcd8dec09016d8d85a35724b3f329860f43372eb426f83778f1df9f019026c7be59bdb7f3049ef3f847cbf817ee48a2cd8b5'
