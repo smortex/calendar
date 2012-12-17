3258ae3d664688a6b6e40d4506831df2fdb9a27b
Romain Tartière <romain@blogreen.org>
2012-12-17 15:09:09 +0100

    Fix display of events starting during a preceding month.

--- test/functional/calendars_controller_test.rb
+++ test/functional/calendars_controller_test.rb
@@ -5,6 +5,14 @@ class CalendarsControllerTest < ActionController::TestCase
     assert_difference('Calendar.count') do
       post :create, :calendar => { :name => 'New Calendar', :color => '#ff0000' }
     end
-    assert_redirected_to calendars_path
+    assert_redirected_to calendar_path(3)
+  end
+
+  test "should show event starting in preceding month" do
+    get :show, :calendar_id => 1, :month => 12, :year => 2012
+    assert_select "a", {:text => "Elbeuf en Bray"}, "This event should be visible"
+
+    get :show, :calendar_id => 1, :month => 1, :year => 2013
+    assert_select "a", {:text => "Elbeuf en Bray"}, "This event should be visible"
   end
 end
